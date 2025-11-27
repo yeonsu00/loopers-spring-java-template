3f02c152b6f470621d61bf80dd3fdbab200388a1
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-11-27 18:49:07 +0900

    feat: 브랜드 조회 기능 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/brand/BrandJpaRepository.java
@@ -0,0 +1,17 @@
+package com.loopers.infrastructure.brand;
+
+import com.loopers.domain.brand.Brand;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BrandJpaRepository extends JpaRepository<Brand, Long> {
+    
+    @Query("SELECT b FROM Brand b WHERE b.id IN :brandIds AND b.deletedAt IS NULL")
+    List<Brand> findByIds(@Param("brandIds") List<Long> brandIds);
+
+    @Query("SELECT b.name FROM Brand b WHERE b.id = :brandId AND b.deletedAt IS NULL")
+    String findNameById(Long brandId);
+}
+
