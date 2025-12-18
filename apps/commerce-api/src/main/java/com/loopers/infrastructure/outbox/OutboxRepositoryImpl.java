aa9ebb41e5bcbf92c90e8fd61a1dd901b7980449
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-12-18 17:43:02 +0900

    feat: Outbox 패턴 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/outbox/OutboxRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.loopers.infrastructure.outbox;
+
+import com.loopers.domain.outbox.Outbox;
+import com.loopers.domain.outbox.OutboxRepository;
+import com.loopers.domain.outbox.OutboxStatus;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class OutboxRepositoryImpl implements OutboxRepository {
+
+    private final OutboxJpaRepository outboxJpaRepository;
+
+    @Override
+    public void saveOutbox(Outbox outbox) {
+        outboxJpaRepository.save(outbox);
+    }
+
+    @Override
+    public List<Outbox> findPendingOutboxes(int limit) {
+        return outboxJpaRepository.findByStatusOrderByCreatedAtAsc(OutboxStatus.PENDING)
+                .stream()
+                .limit(limit)
+                .toList();
+    }
+
+    @Override
+    public List<Outbox> findFailedAndRetryableOutboxes(int limit, int maxRetries) {
+        return outboxJpaRepository.findFailedOutboxesForRetry(OutboxStatus.FAILED, maxRetries)
+                .stream()
+                .limit(limit)
+                .toList();
+    }
+
+    @Override
+    public void markAsPublished(Long id) {
+        outboxJpaRepository.updateOutboxPublishedStatus(id);
+    }
+
+    @Override
+    public void markAsFailed(Long id, String message) {
+        outboxJpaRepository.updateOutboxFailedStatusAndErrorMessageAndRetryCount(id, message);
+    }
+}
+
